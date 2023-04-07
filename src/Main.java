3f76792071c5128987e2ab13147d71526ae548e9
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-04-07 12:23:22 +0530

    BST

--- src/Main.java
+++ src/Main.java
@@ -9,19 +9,18 @@
 //            for a value in a BST can be done in O(log n) time complexity in the average and best cases, where n is
 //            the number of nodes in the tree. This is because each search operation reduces the search space by half,
 //            similar to how binary search works on a sorted array.
-
-
-
+import java.util.*;
 public class Main {
 
-    static class Node{
+    static class Node {
         int data;
         Node left;
         Node right;
 
-        Node(int data){
+        Node(int data) {
             this.data = data;
         }
+    }
         public static Node insert(Node root, int val) {
             if (root == null){
                 root = new Node(val);
@@ -41,12 +40,120 @@ public class Main {
             }
 
             inOrder(root.left);
-            System.out.println(root);
+            System.out.print(root.data + " ");
             inOrder(root.right);
         }
+
+//    Ques2: Search for key in bst
+
+    public static boolean search(Node root, int key) {
+
+        if (root == null){
+            return false;
+        }
+
+        if (root.data > key) {
+            return search(root.left, key);
+        }
+        else if (root.data == key){
+            return true;
+        }
+        else{
+            return search(root.right, key);
+        }
+    }
+//    Ques3: delete node from the BST
+    public static Node delete(Node root, int val) {
+        if (root.data > val) {
+            root.left = delete(root.left, val);
+        }
+        else if (root.data < val){
+            root.right = delete(root.right, val);
+        } else { // root.data == val
+            // Case1
+            if (root.left == null && root.right == null){
+                return null;
+            }
+            // Case 2
+            if (root.left == null) {
+                return root.right;
+            } else if (root.right == null) {
+                return root.left;
+            }
+
+            // case 3
+            Node IS = inOrderSucessor(root.right);
+            root.data = IS.data;
+            root.right = delete(root.right, IS.data);
+        }
+        return root;
+    }
+
+    public static Node inOrderSucessor(Node root) {
+        while (root.left != null){
+            root = root.left;
+        }
+        return root;
+    }
+
+//    Ques4: Print all number in a certain range
+
+    public static void printInRange(Node root, int r1, int r2) {
+
+        if (root == null){
+            return;
+        }
+
+        // Case1
+        if (root.data >= r1 && root.data <= r2) {
+            printInRange(root.left , r1, r2);
+            System.out.print(root.data + " ");
+            printInRange(root.right, r1, r2);
+        }
+        // Case2
+        else if (root.data >= r1){
+            printInRange(root.left, r1, r2);
+        }
+        else {
+            printInRange(root.right, r1, r2);
+        }
+    }
+//  Ques6: Print all paths
+    public static void printAllPaths(Node root, ArrayList<Integer>pathNodes) {
+        if (root == null){
+            return;
+        }
+        pathNodes.add(root.data);
+        if (root.left == null && root.right == null){
+            printPath(pathNodes);
+        } else {
+            printAllPaths(root.left, pathNodes);
+            printAllPaths(root.right, pathNodes);
+        }
+        pathNodes.remove(pathNodes.size()-1);
+    }
+
+    private static void printPath(ArrayList<Integer> pathNodes) {
+        for (int i = 0; i < pathNodes.size(); i++) {
+            System.out.print(pathNodes.get(i) + "->");
+        }System.out.println();
     }
 
     public static void main(String[] args) {
+        int values[] = {8, 5, 3, 6, 10, 11, 14};
+        Node root = null;
 
+        for (int i = 0; i < values.length; i++){
+            root = insert(root, values[i]);
+        }
+        inOrder(root);
+//        System.out.println();
+//
+//       delete(root, 2);
+//       inOrder(root);
+        System.out.println();
+        printInRange(root, 4, 7);
+        System.out.println();
+        printAllPaths(root,new ArrayList<>());
     }
 }
