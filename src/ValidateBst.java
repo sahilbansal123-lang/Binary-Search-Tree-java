e8431f0f895b98ad7262a18b84fcb0f8ee6eff30
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-07-22 12:09:41 +0530

    BST

--- src/ValidateBst.java
+++ src/ValidateBst.java
@@ -1,6 +1,30 @@
 public class ValidateBst {
 
-    public
+    static class Node {
+        Node left;
+        Node right;
+        int data;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static boolean validateBst(Node root, Node min, Node max) {
+        if (root == null) {
+            return true;
+        }
+        if (root != null && root.data <= min.data) {
+            return false;
+        }
+        if (root != null && root.data >= max.data) {
+            return false;
+        }
+
+        return validateBst(root.left, min, root) && validateBst(root.right, root, max);
+    }
 
     public static void main(String[] args) {
 
