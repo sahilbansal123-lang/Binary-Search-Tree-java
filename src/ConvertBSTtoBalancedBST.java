a60efbc8609aabde17d47fe5e819f705bd8780eb
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-07-23 10:44:07 +0530

    BST

---
+++ src/ConvertBSTtoBalancedBST.java
@@ -0,0 +1,20 @@
+public class ConvertBSTtoBalancedBST {
+
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+    public static Node createBst() {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
