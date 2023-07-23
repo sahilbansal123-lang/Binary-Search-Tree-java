c7b42550a027fb39b450b115ce1d6d8b8a9d9a9d
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-07-23 12:00:59 +0530

    BST

---
+++ src/LargestBSTinBT.java
@@ -0,0 +1,55 @@
+public class LargestBSTinBT {
+
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+    static class Info {
+        boolean isBst;
+        int size;
+        int min;
+        int max;
+
+        Info( boolean isBst, int size, int min, int max) {
+            this.isBst = isBst;
+            this.size = size;
+            this.min = min;
+            this.max = max;
+        }
+    }
+
+    public static int maxSize = 0;
+    public static Info largestBst(Node root) {
+        if (root == null) {
+            return new Info(true, 0, Integer.MAX_VALUE, Integer.MIN_VALUE);
+        }
+
+        Info leftInfo = largestBst(root.left);
+        Info rightInfo = largestBst(root.right);
+        int size = leftInfo.size + rightInfo.size + 1;
+        int min = Math.min(root.data, Math.min(leftInfo.min, rightInfo.min));
+        int max = Math.max(root.data, Math.max(leftInfo.max, rightInfo.max));
+
+        // isValid BST condition
+
+        if (root.data <= leftInfo.max && root.data >= rightInfo.min) {
+            return new Info(false, size, min, max);
+        }
+
+        if (leftInfo.isBst && rightInfo.isBst) {
+            maxSize = Math.max(maxSize, size);
+            return new Info(true, size, min, max);
+        }
+
+        return new Info(false, size, min, max);
+    }
+
+
+
+}
