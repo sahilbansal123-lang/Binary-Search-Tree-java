0b88135b3d27d2af5465042aa7d739481d8003ca
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-07-23 10:05:13 +0530

    BST

---
+++ src/SortedArrayToBalancedBst.java
@@ -0,0 +1,20 @@
+public class SortedArrayToBalancedBst {
+
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+    public static Node sortedBsr(int arr[], int strt, int end) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
